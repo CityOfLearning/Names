65af25cb073799c10b377dc6c4a58b07966dc248
Dominic Amato <dominic.a.amato@gmail.com>
2016-07-09 10:24:30 -0500

    update id

--- NamesUI.java
+++ NamesUI.java
@@ -5,7 +5,6 @@ import com.dyn.names.reference.MetaData;
 import com.dyn.names.reference.Reference;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.ModMetadata;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -15,7 +14,7 @@ public class NamesUI {
 	@Mod.Instance(Reference.MOD_ID)
 	public static NamesUI instance;
 
-	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+	@SidedProxy(modId = Reference.MOD_ID, clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
 	public static Proxy proxy;
 
 	@Mod.EventHandler
--- reference/Reference.java
+++ reference/Reference.java
@@ -2,10 +2,9 @@ package com.dyn.names.reference;
 
 public class Reference {
 
-	public static final String MOD_ID = "namesUI";
+	public static final String MOD_ID = "dyn|names";
 	public static final String VERSION = "1.0";
 	public static final String MOD_NAME = "DYN Student name UI";
-	public static final String MINECRAFT_VERSION = "1.8";
 
 	public static final String SERVER_PROXY_CLASS = "com.dyn.names.proxy.Server";
 	public static final String CLIENT_PROXY_CLASS = "com.dyn.names.proxy.Client";
