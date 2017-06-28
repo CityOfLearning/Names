8ea4fff7c8947e97fd89aa74d9352aa1beae8708
Dominic Amato <dominic.a.amato@gmail.com>
2017-06-27 20:40:56 -0500

    create generic notfication

--- manager/NotificationsManager.java
+++ manager/NotificationsManager.java
@@ -4,16 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.dyn.render.hud.notifications.AchievementPlusNotification;
+import com.dyn.render.hud.notifications.GenericNotification;
 import com.dyn.render.hud.notifications.INotification;
 import com.dyn.render.hud.notifications.RequirementNotification;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.ResourceLocation;
 
 public class NotificationsManager {
 	private static final List<INotification> notificationList = new ArrayList<>();
 
 	public static void addAchievementNotification(String title, String subTitle) {
 		notificationList.add(new AchievementPlusNotification(Minecraft.getMinecraft(), title, subTitle));
+	}
+
+	public static void addGenericNotification(ResourceLocation image, String title, String subtitle) {
+		notificationList.add(new GenericNotification(Minecraft.getMinecraft(), image, title, subtitle));
 	}
 
 	public static void addRequirementNotification(String title, String subTitle) {
